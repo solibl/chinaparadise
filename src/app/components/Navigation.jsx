9d4a97d33de7499e7da2a72601a327f3e9bc9955
Brian Lee <brianleedongjun@gmail.com>
2017-12-28 17:38:31 -0800

    Refactor code to shrink Logo into NavBar fixated at top.

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -1,32 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap'
+
 import logo from '../images/logo/CPRlogo.png';
 
 class Navigation extends React.PureComponent {
 	render () {
 		return (
-			<div className="App-header">
-            	<header>
-            	<a href=''><img src={logo} className="App-logo" id='Logo'/></a>
-          		</header>
-				<ul className="Navbar-links">
-					<li>
-						<a href='#'>About Us</a>
-					</li>
-					<li>
-						<a href='#'>Menu</a>
-					</li>
-					<li>
-						<a href='#'>Location</a>
-					</li>
-					<li>
-						<a href='#'>Delivery</a>
-					</li>
-					<li>
-						<a href='#'>Catering</a>
-					</li>
-				</ul>
-        	</div>
+			<div className='App-header'>
+		            <header>
+		            <a href=''><img src={logo} className='App-logo' id='Logo'/></a>
+		          	</header>
+		          	<Navbar id='NavMenu'>
+		          	</Navbar>
+	        </div>
 		);
 	}
 }
